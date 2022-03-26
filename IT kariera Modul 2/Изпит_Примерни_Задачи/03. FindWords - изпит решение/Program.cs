f3d2af01ebb18c5c7c9e0b0d9fd2b642913522a2
Tsvetoslav <cveti3060@gmail.com>
2022-03-26 15:56:04 +0200

    3

--- IT kariera Modul 2/Изпит_Примерни_Задачи/03. FindWords - изпит решение/Program.cs
+++ IT kariera Modul 2/Изпит_Примерни_Задачи/03. FindWords - изпит решение/Program.cs
@@ -3,50 +3,36 @@ using System.Linq;
 using System.Text;
 using System.Collections.Generic;
 
-class Program
+class MagicNumbers
 {
-    static void Print(List<string> list)
-    {
-        for (int i = 0; i < list.Count; i++)
-        {
-            if (i == list.Count - 1)
-            {
-                Console.Write(list[i]);
-            }
-            else
-            {
-                Console.Write(list[i] + ", ");
-            }
-        }
-    }
+
     static void Main(string[] args)
     {
         int n = int.Parse(Console.ReadLine());
         string[] arr = new string[n];
         List<string> special = new List<string>();
         List<string> other = new List<string>();
-        int condition = 0;
+        int br = 0;
         for (int i = 0; i < n; i++)
         {
             arr[i] = Console.ReadLine();
-            if ('A' <= arr[i][0] && arr[i][0] <= 'Z')
+            if (arr[i][0] >= 'A' && arr[i][0] <= 'Z')
             {
-                condition++;
+                br++;
             }
             for (int j = 0; j < arr[i].Length; j++)
             {
                 if (arr[i][j] == 'i')
                 {
-                    condition++;
+                    br++;
                     break;
                 }
             }
             if (arr[i].Length > 7)
             {
-                condition++;
+                br += 1;
             }
-            
-            if (condition == 3)
+            if (br == 3)
             {
                 special.Add(arr[i]);
             }
@@ -54,13 +40,33 @@ class Program
             {
                 other.Add(arr[i]);
             }
-            condition = 0;
+            br = 0;
         }
         Console.Write("Special words: ");
-        Print(special);
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (i == list.Count - 1)
+            {
+                Console.Write(special[i]);
+            }
+            else
+            {
+                Console.Write(special[i] + ", ");
+            }
+        }
         Console.WriteLine();
         Console.Write("Other words: ");
-        Print(other);
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (i == list.Count - 1)
+            {
+                Console.Write(other[i]);
+            }
+            else
+            {
+                Console.Write(other[i] + ", ");
+            }
+        }
         Console.WriteLine();
 
     }
